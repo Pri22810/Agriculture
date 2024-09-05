7f8e9d3dd35015e5af104a40771f369f6e563042
Pri22810 <138323102+Pri22810@users.noreply.github.com>
2024-09-05 23:45:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Agriculture
+# Soil Recovery Agriculture: "Crop Rotation Predictions"
+
+A simple ML based website which recommends the best crop to grow for better yield and also crops to grow to increase fertilty of soil after money crop harvest.
+
+
+
+Money Crop prediction:-
+
+Recommendation about the type of crops to be cultivated which is best suited for the respective conditions by using Machine Learning models like Random Forest,Decision Tree
+
+Website:-
+
+Built an Website combined with ML models by using Flask.
+The website consists of details of need for crop Rotation for imporving Soil fertility.
+The farmers gets best crops to groiw prediction by entering nutrients values like ph,N,P,K,water quantity per hecter and location
+The prediction involves 2 to 4 eligible best crops to grow in the land for given nutrients,position,humidity.Farmer will select his demanding crop from the prediction.
+After Selection the website gives top irrigation methods for the selected crop and soil fertility recovery crops after harvesting the selected crop
---
+++ app/app.py
@@ -0,0 +1,114 @@
+# Importing essential libraries and modules
+
+from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+from utils.disease import disease_dic
+from utils.fertilizer import fertilizer_dic
+import requests
+import config
+import pickle
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from utils.model import ResNet9
+# ==============================================================================================
+
+# -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
+
+
+# Loading crop recommendation model
+
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+# =========================================================================================
+
+# Custom functions for calculations
+
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+
+# ===============================================================================================
+# ------------------------------------ FLASK APP -------------------------------------------------
+
+
+app = Flask(__name__)
+
+# render home page
+
+
+@ app.route('/')
+def home():
+    title = 'Harvestify - Home'
+    return render_template('index.html', title=title)
+
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Harvestify - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# ===============================================================================================
+
+# RENDER PREDICTION PAGES
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+# ===============================================================================================
+if __name__ == '__main__':
+    app.run(debug=False)
